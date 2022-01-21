e078404c962d96009bf891c12a80971a940d2f5f
aziz <aziz@silkroadprofessionals.com>
2022-01-21 10:45:43 +0500

    Test routing

---
+++ Pages/TypeParamComponent.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorApp.Pages
+{
+    public partial class TypeParamComponent<TValue>
+    {
+		public List<TValue> GenericList { get; set; }
+	}
+}
